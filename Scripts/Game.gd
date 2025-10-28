65f5a4e90e3265efb042ef67737f0555a70d0bc7
mjshsgamedev <mjshsgamedev@gmail.com>
2025-10-28 13:20:50 -0700

    instantiate the player at the position of the StartPoint Node

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@export var StartPoint : Node2D
+
 var Player : Node2D
-var StartPoint : Node2D
 
 func _ready():
-	pass #TODO instantiate player and startpoint
+	Player = preload("res://Player/Player.tscn").instantiate()
+	Player.position = StartPoint.position
+	add_child(Player)
 
 func _process(delta: float):
 	pass
