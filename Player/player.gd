651542f1192ac7f2f5fe0202496f8d27a9c9e19c
mjshsgamedev <mjshsgamedev@gmail.com>
2025-11-04 13:22:36 -0800

    add movement stuff (not working)

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,16 @@
-extends Node
+extends RigidBody2D
+
+@export var speed = 100.0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	var direction : Vector2 = Vector2(
+		(float(Input.is_action_pressed("mov_right")) -
+			float(Input.is_action_pressed("mov_left"))),
+		(float(Input.is_action_pressed("mov_up")) -
+			float(Input.is_action_pressed("mov_down")))
+	)
+	
+	move_and_collide(direction * speed)
