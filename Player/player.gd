c5d66af52eb99ae6aed6d6e9a914c3f0da4f372f
mjshsgamedev <mjshsgamedev@gmail.com>
2025-11-21 13:22:28 -0800

    start retooling move system

--- Player/player.gd
+++ Player/player.gd
@@ -3,25 +3,25 @@ extends RigidBody2D
 @export var speed = 300.0
 
 var dampening_mul = 1 + speed / 1000
+var pivot_div = 10
+var last_direction = Vector2.ZERO
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	var direction : Vector2 = Vector2(
+	var direction = Vector2(
 		(float(Input.is_action_pressed("mov_right")) -
 			float(Input.is_action_pressed("mov_left"))),
 		(float(Input.is_action_pressed("mov_down")) -
 			float(Input.is_action_pressed("mov_up")))
 	)
 	
-	#move_and_collide(direction * speed)
-	if(direction.x == 0):
-		linear_velocity.x /= dampening_mul
-	else:
-		linear_velocity.x = direction.x * speed
+	if direction.x == -last_direction.x: linear_velocity.x /= pivot_div
+	if direction.y == -last_direction.y: linear_velocity.y /= pivot_div
 	
-	if(direction.y == 0):
-		linear_velocity.y /= dampening_mul
-	else:
-		linear_velocity.y = direction.y * speed
+	add_constant_central_force(direction * speed)
+	linear_velocity /= dampening_mul
+	
+	if direction.x != 0: last_direction.x = direction.x
+	if direction.y != 0: last_direction.y = direction.y
